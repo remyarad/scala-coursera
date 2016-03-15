ff4148eb4bf17c66d47e196fc7387538fc1fc5be
remyarad <remyarad@gmail.com>
2016-03-15 23:38:58 +1100

    Update TailRecursion.scala

--- src/main/scala/TailRecursion.scala
+++ src/main/scala/TailRecursion.scala
@@ -8,7 +8,17 @@ object TailRecursion {
       if(n==0)acc  else loop(acc*n,n-1)
     loop(1,n)
   }
+  
+  def sum(f:Int=>Int)(a:Int,b:Int):Int={
+    def loop(a:Int,acc:Int):Int=
+      if(a>b)acc else loop(a+1,f(a)+acc)
+    loop(a,0)
+
+  }
+  def cube(x: Int):Int=x*x*x
+  
   def main (args: Array[String]){
     println("Factorial of 5 is: "+factorial(5))
+    println("Sum of cubes: "+sum(cube)(1,3))  
   }
 }
